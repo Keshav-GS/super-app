9525167302fb585d6721fe62d51fc2c7cddd50c0
priyanshuranjan03 <priyanshuranjan13032003@gmail.com>
2025-05-16 11:43:35 +0530

    config files updated

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,5 +1,5 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-
+import * as webpack from 'webpack';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
@@ -7,4 +7,7 @@ export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
   }),
+  // new webpack.DefinePlugin({
+  //   'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+  // }),
 ];
--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -17,3 +17,57 @@ export const rendererConfig: Configuration = {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
   },
 };
+
+// import type { Configuration } from 'webpack';
+
+// import { rules } from './webpack.rules';
+// import { plugins } from './webpack.plugins';
+// import * as path from 'path';
+// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+
+// const isDevelopment = process.env.NODE_ENV !== 'production';
+// rules.push({
+//   test: /\.css$/,
+//   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'postcss-loader' }],
+// });
+// rules.push({
+//   test: /\.(ts|tsx)$/,
+//   exclude: /node_modules/,
+//   use: [
+//     {
+//       loader: require.resolve('babel-loader'),
+//       options: {
+//         presets: [
+//           '@babel/preset-env',
+//           '@babel/preset-react',
+//           '@babel/preset-typescript',
+//         ],
+//         plugins: [
+//           isDevelopment && require.resolve('react-refresh/babel'),
+//         ].filter(Boolean),
+//       },
+//     },
+//   ],
+// });
+// export const rendererConfig: Configuration = {
+//   mode: isDevelopment ? 'development' : 'production',
+//   module: {
+//     rules,
+//   },
+//   plugins: [
+//     ...plugins,
+//     ...(isDevelopment ? [new ReactRefreshWebpackPlugin()] : []),
+//   ],
+//   resolve: {
+//     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+//   },
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'dist'),
+//     },
+//     historyApiFallback: true, // needed for React Router + BrowserRouter
+//     hot: true,
+//     port: 3000,
+//   },
+//   devtool: isDevelopment ? 'inline-source-map' : false,
+// };
